806e0891608b7278c2dc0cdff706f82ce3d3780c
Hunter Fuller <hfuller@pixilic.com>
2017-03-09 23:54:33 -0600

    add webui player for weup playlist and random songs from history

--- webui.py
+++ webui.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import json
 import sqlite3
 from flask import Flask, redirect, request
+import youtube_dl
 
 app = Flask(__name__)
 
@@ -23,9 +24,22 @@ def get_plays_json():
 	skip = request.args['skip']
 	print("Retrieving plays after", skip)
 	cur.execute('''SELECT * FROM plays WHERE played_at > ? ORDER BY played_at DESC LIMIT 1;''', [skip])
-	return json.dumps(row_to_dict(cur.fetchone()))
+	row = cur.fetchone()
+	if row == None:
+		return json.dumps(row)
+	return json.dumps(row_to_dict(row))
 
-@app.route('/plays/random')
+@app.route('/plays/<played_at>/youtube')
+def get_play_youtube(played_at):
+	cur.execute('''SELECT * FROM plays WHERE played_at = ?;''', [played_at])
+	row = cur.fetchone()
+	print(row)
+	try:
+		return ydl.extract_info("gvsearch1:" + row["artist"] + " " + row["song"], False)["entries"][0]["id"]
+	except:
+		return ydl.extract_info("gvsearch1:" + row["song"], False)["entries"][0]["id"]
+
+@app.route('/plays/random.json')
 def get_random_json():
 	cur.execute('''SELECT * FROM plays ORDER BY RANDOM() LIMIT 1;''')
 	return json.dumps(row_to_dict(cur.fetchone()))
@@ -39,6 +53,9 @@ if __name__ == "__main__":
 	print("Starting DB")
 	db = sqlite3.connect('weupinthis.sqlite3')
 	db.row_factory = sqlite3.Row
+
+	print("init youtube-dl")
+	ydl = youtube_dl.YoutubeDL({})
 
 	db.execute('''CREATE TABLE IF NOT EXISTS plays(id INTEGER PRIMARY KEY AUTOINCREMENT, played_at TEXT, artist TEXT, song TEXT);''')
 	cur = db.cursor()
